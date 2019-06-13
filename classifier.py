e5f062840441573446ff5d1ae9156518cef007dc
unknown <hiago7mg@gmail.com>
2019-06-13 02:20:40 -0300

    Update classifier.py to match notebook

--- classifier.py
+++ classifier.py
@@ -2,8 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skimage
 import os
+import random
 from sklearn import svm
 from skimage import io, color, feature, data
+
 
 def show(img):
     fig, ax = plt.subplots(1, 1, figsize=(5, 5))
@@ -14,21 +16,29 @@ def show(img):
 image_dir_path = "frutas_dataset_train"
 image_list = []
 labels = []
-count = 0
+image_limit_per_folder = 10
+
 ##Reads each image in subdirectories and stores it in image_list
 ##The directory names are used as labels for classification
-print("reading images")
+current_image_count = 0
 for image_dir in os.listdir(image_dir_path):
     full_path = os.path.join(image_dir_path,image_dir)
     
     if os.path.isdir(full_path):
         for image_file in os.listdir(full_path):
-            print(os.path.join(full_path,image_file))
             image = io.imread(os.path.join(full_path,image_file))
             image_list.append(image)
             labels.append(image_dir)
+            current_image_count += 1
+            if(current_image_count >= image_limit_per_folder):
+                current_image_count = 0
+                break
+
+print(labels)
 #Takes about a billion years to be done
 
+#for image in images:
+#    show(image)
 def extract_lbp(image,radius,n_points):
     ##Function for lbp feature extraction
     gray_image = color.rgb2gray(image)
@@ -37,7 +47,6 @@ def extract_lbp(image,radius,n_points):
                         range=(0, n_points),normed = True)[0]
     return hist
 
-print("extracting features")
 radius = 3
 n_points = radius * 8
 
@@ -45,13 +54,21 @@ datasets = []
 for image in image_list:
     datasets.append(extract_lbp(image,radius,n_points))
 
-print("svm")
 ##SVM model generation
-model = svm.LinearSVC(C=100.0, random_state=42,max_iter = 10000)
+model = svm.LinearSVC(C=100)
 model.fit(datasets,labels)
 
 ##Test
-test_image = io.imread("frutas_dataset_train/kiwi/kiwi_001.jpg")
-test_data = extract_lbp(test_image,radius,n_points) 
-predict = model.predict([test_data])
-print("Prediction " + str(predict[0]))
+test_image_count = 20
+test_images = []
+expected = []
+
+predict = model.predict(datasets)
+
+correct = 0
+for i in range(len(predict)):
+    print("Prediction: " + predict[i] + "  Expected: " + labels[i])
+    if(predict[i] == labels[i]):
+        correct += 1
+        
+print(str(correct) + "/" + str(len(predict)))
