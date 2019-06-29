28e2f79d9bed8de909d2161fd39d67ccdde0a838
unknown <hiago7mg@gmail.com>
2019-06-29 20:07:06 -0300

    Prints

--- classifier_train.py
+++ classifier_train.py
@@ -26,9 +26,12 @@ def read_data(image_dir_path):
 
 
 image_dir_path = "frutas_dataset_train"
+print("READING DATA")
 fruits, labels = read_data(image_dir_path)
+print("DONE")
+print("SPLITTING TEST")
 fruits_train, fruits_test, labels_train, labels_test = train_test_split(fruits, labels, random_state=2010, test_size=0.5)
-
+print("DONE")
 
 def create_heatmap(true, predict, labels):
 	cfmx = confusion_matrix(true, predict, labels)
@@ -61,7 +64,7 @@ def create_heatmap(true, predict, labels):
 	ax.set_xticklabels(strategy_path,minor=False,fontsize=12,rotation=90)
 	ax.set_yticklabels(strategy_path,minor=False,fontsize=12)
 
-	plt.savefig('heatmap_CH.pdf', bbox_inches='tight')
+	plt.savefig('heatmap_HOG.pdf', bbox_inches='tight')
 	plt.show()
 
 def train_model(feature_set, test_set, labels_train, labels_test, model_name, labels):
@@ -69,23 +72,42 @@ def train_model(feature_set, test_set, labels_train, labels_test, model_name, la
     model = KNeighborsClassifier(n_neighbors=1)
     model.fit(feature_set,labels_train)
     
+    print("DONE")
     joblib.dump(model, 'trained_models/' + model_name)
     
+    print("TESTING MODEL")
     predict = model.predict(test_set)
-    
+    print("DONE")
+
+    print("GENERATING HEATMAP")
     single_labels = list(set(labels))
     create_heatmap(labels_test, predict, single_labels)
+    print("DONE")
 
 
 def generate_predictor(train_set, test_set, labels_train, labels_test, feature_extraction_method, model_name):
     feature_set = []
+
+    print("EXCTRACTING TRAIN FEATURES")
+    i=0
     for image in train_set:
+        print(str(i) + " train feat " + image)
         feature_set.append(feature_extraction_method(io.imread(image)))
-    
+        i+=1
+
+    print("DONE")
+    print("EXTRACTING TEST FEATURES")
+    i=0
     test_feature_set = []
+        
     for image in test_set:
+        print(str(i) + " test feat " + image)
         test_feature_set.append(feature_extraction_method(io.imread(image)))
+        i+=1
+
+    print("DONE")
     
+    print("TRAINING MODEL")
     train_model(feature_set, test_feature_set, labels_train, labels_test, model_name, labels)
 
 def extract_colorHist(image):
@@ -119,6 +141,6 @@ def extract_lbp(image):
                         range=(0, n_points + 2),density = True)[0]
     return hist
 
-generate_predictor(fruits_train, fruits_test, labels_train, labels_test, extract_colorHist, 'knn_colorHist.pkl')
-#generate_predictor(fruits_train, fruits_test, labels_train, labels_test, extract_hog, 'knn_hog.pkl')
+#generate_predictor(fruits_train, fruits_test, labels_train, labels_test, extract_colorHist, 'knn_colorHist.pkl')
+generate_predictor(fruits_train, fruits_test, labels_train, labels_test, extract_hog, 'knn_hog.pkl')
 #generate_predictor(fruits_train, fruits_test, labels_train, labels_test, extract_lbp, 'knn_lbp.pkl')
