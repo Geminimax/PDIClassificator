6297b7b9d0e5a3bcff283b27f19a67634e5e749c
Adler Melgaço Ferreira <adlermf07@gmail.com>
2019-06-29 19:49:37 -0300

    extraction methods

--- script_gui.py
+++ script_gui.py
@@ -1,19 +1,33 @@
-import classifier_train
-import joblib
+def extract_colorHist(image):
+    img_f = skimage.img_as_float(image)
+    hist_red = exposure.histogram(img_f[:,:,0].flatten(), nbins=8, normalize=True)[0]
+    hist_green = exposure.histogram(img_f[:,:,1].flatten(), nbins=8, normalize=True)[0]
+    hist_blue = exposure.histogram(img_f[:,:,2].flatten(), nbins=8, normalize=True)[0]
+    hist = []
+    hist.extend(hist_red)
+    hist.extend(hist_green)
+    hist.extend(hist_blue)
 
-def single_image_predict(image, descriptor, path):
+    #hist = plt.hist(img_f.flatten(),bins=np.arange(0,8),normed = True)[0]
+    return hist
 
-	features_image = []
-	if descriptor == "LBP":
-		features_image.append(classifier_train.extract_lbp(image))
-	elif descriptor == "HOG":
-		features_image.append(classifier_train.extract_hog(image))
-	elif descriptor == "ColorHist":
-		features_image.append(classifier_train.extract_colorHist(image))
+## Function for hog feature extraction
+def extract_hog(image):
+    # feat is the feacture vector
+    feat, hog_image = feature.hog(image, visualize=True, feature_vector=True)
+    # show(hog_image)
+    # print(feat)
+    return feat
 
-	model = joblib.load(path)
-	predict = model.predict(features_image)
-	return predict
+def extract_lbp(image):
+    radius = 3
+    n_points = radius * 8
+    ##Function for lbp feature extraction
+    gray_image = color.rgb2gray(image)
+    lbp = feature.local_binary_pattern(gray_image, n_points, radius)
+    hist = plt.hist(lbp.ravel(),bins=np.arange(0, n_points + 3),
+                        range=(0, n_points + 2),density = True)[0]
+    return hist
 
 def read_tests(image_dir_path):
     fruits_test = []
